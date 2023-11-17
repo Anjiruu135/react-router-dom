a8b7e7e2fce586de0b131a18665ff72ee1d82d8c
Tedd Angelo Gitalan <gitalan.tedd@gmail.com>
2023-11-17 18:04:12 +0800

    Update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,137 @@
-import React from 'react'
+import React from "react";
 import { useFormik } from "formik";
-import { RegisterSchema } from '../Validations/UserValidation.js';
+import { RegisterSchema } from "../Validations/UserValidation.js";
 
 function Register() {
-    const RegisterUser = async (event) => {
-        event.preventDefault()
-        let formData = {
-            name: event.target[0].value,
-            username: event.target[1].value,
-            email: event.target[2].value,
-            password: event.target[3].value,
-            confirmpassword: event.target[4].value,
-        };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: (values) => {
+      formik.validateForm().then((errors) => {
+        if (Object.keys(errors).length === 0) {
+          window.alert(`User "${values.username}" Registered Successfully`);
+          console.log(values);
+        } else {
+          const errorMessages = Object.values(errors).join("\n");
+          window.alert(`Validation Error:\n${errorMessages}`);
+        }
+      });
+    },
+  });
 
-        RegisterSchema.validate(formData)
-        .then(validatedData => {
-            const { username } = validatedData;
-            window.alert(`User "${username}" Registered Succesfully`);
-            console.log(formData)
-        })
-        .catch(validationError => {
-            window.alert(validationError.message);
-        });
+  const inputStyles = (field) => {
+    return {
+      border:
+        formik.touched[field] && formik.errors[field]
+          ? "1px solid red"
+          : "1px solid #ccc",
+      color: formik.touched[field] && formik.errors[field] ? "red" : "inherit",
     };
+  };
+
+  const errorMessageStyles = {
+    height: "1.5em",
+  };
 
   return (
     <div>
       <h1>REGISTER</h1>
-      <form onSubmit={RegisterUser}>
-        <label htmlFor="name">Name: </label>
-        <input type="text" id="name" placeholder='Name'/>
-        <label htmlFor="username">Username: </label>
-        <input type="text" id="username" placeholder='Username'/>
-        <label htmlFor="email">Email: </label>
-        <input type="text" id="email" placeholder='email@email.com'/>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" placeholder='Password'/>
-        <label htmlFor="confirmpassword">Confirm Password: </label>
-        <input type="text" id="confirmpassword" placeholder='Confirm Password'/>
-        <input className='button' type="submit" value="Register"></input>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="container-box">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+            style={inputStyles("name")}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div style={{ color: "red", fontSize: "14px"}}>{formik.errors.name}</div>
+          ) : null}
+        </div>
+
+        <div className="container-box">
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.username}
+            style={inputStyles("username")}
+          />
+          {formik.touched.username && formik.errors.username ? (
+            <div style={{ color: "red", fontSize: "14px"}}>{formik.errors.username}</div>
+          ) : null}
+        </div>
+
+        <div className="container-box">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="email@email.com"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            style={inputStyles("email")}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div style={{ color: "red", fontSize: "14px"}}>{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <div className="container-box">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            style={inputStyles("password")}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div style={{ color: "red", fontSize: "14px"}}>{formik.errors.password}</div>
+          ) : null}
+        </div>
+
+        <div className="container-box">
+          <label htmlFor="confirmpassword">Confirm Password: </label>
+          <input
+            type="password"
+            id="confirmpassword"
+            name="confirmpassword"
+            placeholder="Confirm Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.confirmpassword}
+            style={inputStyles("confirmpassword")}
+          />
+          {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
+            <div style={{ color: "red", fontSize: "14px"}}>{formik.errors.confirmpassword}</div>
+          ) : null}
+        </div>
+
+        <input className="button" type="submit" value="Register"></input>
       </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
